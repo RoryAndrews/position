464d47fd6e0eb93d936c25da1d6894bb0d9a18fa
Rory Andrews <rory00@gmail.com>
2015-10-05 21:36:55 -1000

    Deleted intelliJ files and added gitignore

--- position.js
+++ position.js
@@ -2,7 +2,7 @@
  * Created by Rory on 9/23/2015.
  */
 
-var testlist = [1, 2, 4, 7];
+var testlist = [1, 3, 5, 6];
 
 function position(list, target) {
   for(var i = 0; i < list.length; i++) {
@@ -13,4 +13,25 @@ function position(list, target) {
   return list.length;
 }
 
-console.log(position(testlist, 8));
+function binaryPosition(list, target) {
+  var mid = Math.floor((list.length) / 2);
+  console.log("MID: " + mid);
+  if(list[mid] >= target) {
+    if(mid = 0) {
+      return mid;
+    }
+    else {
+      return Math.min(mid, binaryPosition(list.slice(0, mid)));
+    }
+  }
+  else {
+    if (mid + 1 != list.length) {
+      return mid + 1 + binaryPosition(list.slice(mid + 1, list.length));
+    }
+  }
+}
+
+console.log(binaryPosition(testlist, 5));
+console.log(binaryPosition(testlist, 2));
+console.log(binaryPosition(testlist, 7));
+console.log(binaryPosition(testlist, 0));
